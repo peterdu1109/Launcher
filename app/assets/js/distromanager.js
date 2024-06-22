3cc2ac6915c9ffe2f9fdc2b777711b20cb0d15d2
Shisuiicaro <gnarnd4531@gmail.com>
2024-06-21 22:31:01 -0300

    rebrand

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,8 @@ const { DistributionAPI } = require('hasta-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://zelthoriaismp.cloud/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://zelthoriaismp.cloud/nebula/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
